d046cedc69092ed3ae85aa2f720626ff470329a5
Ed Bent <109971677+edbentho@users.noreply.github.com>
2025-04-11 14:49:50 +0100

    add missing description var (#3)

--- modules/secrets_manager/secret/main.tf
+++ modules/secrets_manager/secret/main.tf
@@ -1,6 +1,7 @@
 resource "aws_secretsmanager_secret" "team_secret" {
-  name       = var.name
-  kms_key_id = try(var.kms_key_id, null)
+  description = var.description
+  name        = var.name
+  kms_key_id  = try(var.kms_key_id, null)
 }
 
 resource "aws_secretsmanager_secret_version" "team_secret" {
