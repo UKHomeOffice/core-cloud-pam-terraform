1462b611da921778e096990d97e1c60137270d0d
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-04-11 10:51:15 +0100

    update modules and add opensource files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # core-cloud-pam-terraform
+This repo contains Terraform modules for resources required as prerequisites to the AWS Privileged Access Management (PAM) solution - TEAM
+https://aws-samples.github.io/iam-identity-center-team/docs/deployment/prerequisites.html
+
--- modules/secrets_manager/secret/main.tf
+++ modules/secrets_manager/secret/main.tf
@@ -1,4 +1,10 @@
 resource "aws_secretsmanager_secret" "team_secret" {
-  name = var.name
+  description = var.description
   kms_key_id = try(var.kms_key_id, null)
+  name = var.name
+}
+
+resource "aws_secretsmanager_secret_version" "team_secret" {
+  secret_id     = aws_secretsmanager_secret.team_secret.id
+  secret_string = jsonencode(var.secret_data)
 }
