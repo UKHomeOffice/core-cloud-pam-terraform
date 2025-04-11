d645db753340b329e1c3ce86aeb4f85bc470b755
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-04-11 11:02:28 +0100

    update secret module

--- modules/secrets_manager/secret/variables.tf
+++ modules/secrets_manager/secret/variables.tf
@@ -1,9 +1,20 @@
+variable "description" {
+  description = "Description of the secret"
+  type        = string
+}
+
 variable "name" {
   description = "Name of the secret"
-  type = string
+  type        = string
 }
 
 variable "kms_key_id" {
   description = "ARN of the KMS key to encrypt the secret"
-  type = string
+  type        = string
+}
+
+variable "secret_data" {
+  description = "Secret value"
+  type        = map(string)
+  sensitive   = true
 }
