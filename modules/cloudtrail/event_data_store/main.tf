5c4eae96f6a55192c9cef4679c0bb63676eb097b
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-06-10 13:26:04 +0100

    update cloudtrail module

--- modules/cloudtrail/event_data_store/main.tf
+++ modules/cloudtrail/event_data_store/main.tf
@@ -1,4 +1,7 @@
 resource "aws_cloudtrail_event_data_store" "team_data_store" {
   name                           = var.name
+  organization_enabled           = var.organization_enabled
+  retention_period               = var.retention_period
+  tags                           = var.tags
   termination_protection_enabled = true
 }
--- modules/cloudtrail/event_data_store/variables.tf
+++ modules/cloudtrail/event_data_store/variables.tf
@@ -2,3 +2,18 @@ variable "name" {
   description = "Name of the Event Data Store"
   type        = string
 }
+
+variable "organization_enabled" {
+  description = "Collects events logged for the whole organization"
+  type        = bool
+}
+
+variable "retention_period" {
+  description = "The retention period of the event data store, in days."
+  type        = number
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+}
