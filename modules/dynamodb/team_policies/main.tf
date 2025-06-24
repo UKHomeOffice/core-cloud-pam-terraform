31049a1f583ceb73fcc8073429ddeb929ed9b835
Ed Bent <109971677+edbentho@users.noreply.github.com>
2025-06-24 16:07:22 +0100

    CCL-3367 - TEAM policies (#13)
    * adding dynamodb module for TEAM policies
    
    * rename variable approvers_policies
    
    * correct account param names, and typename
    
    * remove debug outputs
    
    * hardcode timestamps
    
    * remove DS_Store

---
+++ modules/dynamodb/team_policies/main.tf
@@ -0,0 +1,160 @@
+data "aws_dynamodb_table" "eligibility_table" {
+  name = var.eligibility_table_name
+}
+
+data "aws_dynamodb_table" "approvers_table" {
+  name = var.approvers_table_name
+}
+
+data "aws_ssoadmin_instances" "this" {}
+
+data "aws_identitystore_group" "approvers_group" {
+  # for_each = toset(var.approver_policies)
+  for_each = { for policy in var.approvers_policies : policy.group_name => policy }
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.value.group_name
+    }
+  }
+}
+
+data "aws_identitystore_group" "eligibility_group" {
+  for_each = { for policy in var.eligibility_policies : policy.group_name => policy }
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.value.group_name
+    }
+  }
+}
+
+data "aws_ssoadmin_permission_set" "this" {
+  for_each = { for policy in var.eligibility_policies : policy.group_name => policy }
+
+  instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  name         = each.value.permissions
+}
+
+locals {
+  eligibility_items = {
+    for idx, policy in var.eligibility_policies : idx => jsonencode({
+      id = {
+        S = data.aws_identitystore_group.eligibility_group[policy.group_name].group_id
+      },
+      accounts = {
+        L = [
+          for account in policy.accounts : {
+            M = {
+              id   = { S = tostring(account.account_id) },
+              name = { S = account.account_name }
+            }
+          }
+        ]
+      },
+      approvalRequired = {
+        BOOL = policy.approval_required
+      },
+      createdAt = {
+        S = "2025-06-24T09:00:00Z"
+      },
+      duration = {
+        S = "${tostring(policy.duration)}"
+      },
+      modifiedBy = {
+        S = "Terraform"
+      },
+      name = {
+        S = "${data.aws_identitystore_group.eligibility_group[policy.group_name].display_name}"
+      },
+      ous = {
+        L = []
+      },
+      permissions = {
+        L = [
+          {
+            M = {
+              id   = { S = data.aws_ssoadmin_permission_set.this[policy.group_name].id },
+              name = { S = policy.permissions }
+            }
+          }
+        ]
+      }
+      ticketNo = {
+        S = "${policy.ticket_no}"
+      },
+      type = {
+        S = "Group"
+      },
+      updatedAt = {
+        S = "2025-06-24T09:00:00Z"
+      },
+      __typename = {
+        S = "Eligibility"
+      }
+    })
+  }
+}
+
+locals {
+  approvers_items = {
+    for i, policy in var.approvers_policies : i => jsonencode({
+      id = {
+        S = tostring(policy.account_id)
+      },
+      approvers = {
+        L = [
+          { S = data.aws_identitystore_group.approvers_group[policy.group_name].display_name }
+        ]
+      },
+      createdAt = {
+        S = "2025-06-24T09:00:00Z"
+      },
+      groupIds = {
+        L = [
+          { S = data.aws_identitystore_group.approvers_group[policy.group_name].group_id }
+        ]
+      },
+      modifiedBy = {
+        S = "Terraform"
+      },
+      name = {
+        S = policy.account_name
+      },
+      ticketNo = {
+        S = policy.ticket_no
+      },
+      type = {
+        S = "Account"
+      },
+      updatedAt = {
+        S = "2025-06-24T09:00:00Z"
+      },
+      __typename = {
+        S = "Approvers"
+      }
+    })
+  }
+}
+
+resource "aws_dynamodb_table_item" "eligibility" {
+  for_each = local.eligibility_items
+
+  table_name = data.aws_dynamodb_table.eligibility_table.name
+  hash_key   = data.aws_dynamodb_table.eligibility_table.hash_key
+  item       = each.value
+}
+
+resource "aws_dynamodb_table_item" "approvers" {
+  for_each = local.approvers_items
+
+  table_name = data.aws_dynamodb_table.approvers_table.name
+  hash_key   = data.aws_dynamodb_table.approvers_table.hash_key
+  item       = each.value
+}
---
+++ modules/dynamodb/team_policies/variables.tf
@@ -0,0 +1,19 @@
+variable "approvers_policies" {
+  description = "list of approval policies"
+  type        = list
+}
+
+variable "approvers_table_name" {
+  description = "Name of the Approvers Policy table"
+  type        = string
+}
+
+variable "eligibility_policies" {
+  description = "list of eligibility policies"
+  type        = list
+}
+
+variable "eligibility_table_name" {
+  description = "Name of the Eligibility Policy table"
+  type        = string
+}
